cb5823e5a9e25f9e86106d5da0a49424166dff5d
Terry Thomas <terry.t@teachingstrategies.com>
2025-10-23 09:30:37 -0500

    update dashboard UI calls

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,19 @@ class Booking < ApplicationRecord
   has_and_belongs_to_many :dogs
 
   enum :booking_status, { pending: 0, confirmed: 1, cancelled: 2, completed: 3 }
+
+  def owner_display_name
+    return "No Owner" unless owner
+    "#{owner.first_name} #{owner.last_name[0]}."
+  end
+
+  def sitter_display_name
+    return "No Sitter" unless sitter
+    "#{sitter.first_name} #{sitter.last_name[0]}."
+  end
+
+  def dog_names
+    return "No Dogs" unless dogs
+    dogs.pluck(:name).join(", ")
+  end
 end
